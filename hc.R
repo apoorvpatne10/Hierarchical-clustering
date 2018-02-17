90251f8b5f5ca568d0e24682992296ff9d5f934e
Apoorv Patne <31184004+apoorvpatne10@users.noreply.github.com>
2018-02-17 15:04:39 +0530

    Add files via upload

---
+++ hc.R
@@ -0,0 +1,24 @@
+# Hierarchical Clustering
+
+# Importing the dataset
+dataset = read.csv('Mall_Customers.csv')
+X = dataset[4:5]
+
+# Using the dendrogram to find the optimal no of clusters
+dendrogram = hclust(dist(X, method = 'euclidean'),
+                    method = 'ward.D')
+plot(dendrogram, main = paste('Dendrogram'),
+     xlab = 'Customers',
+     ylab = 'Eclidean distances')
+
+# Fitting hierarchical clustering to the mall dataset 
+hc = hclust(dist(X, method = 'euclidean'), method = 'ward.D')
+y_hc = cutree(hc, 5)
+
+# Visualizing the clusters 
+library(cluster)
+clusplot(X, y_hc, lines = 0, shade = TRUE, color = TRUE,
+         labels = 2, plotchar = FALSE, span = TRUE,
+         main = paste('Cluster of clients'), xlab = "Annual Income",
+         ylab = "Spending Score")
+
